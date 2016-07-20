ceeb9e424385c003c7d77c54d872ce47f2be2d1c
nrakochy <nick.rakochy@gmail.com>
2016-07-20 14:07:55 -0500

    Updated IO to partition lazy seq. Still writes one at a time

--- src/secured/file_io.clj
+++ src/secured/file_io.clj
@@ -2,6 +2,9 @@
   (:require [clojure.java.io :as io :refer [reader file]])
   (:require [clojure.string :as s :refer [split-lines split lower-case replace]]))
 
+(defn data-newline [line write-type]
+  (str line "\r\n"))
+
 (def password-data-path "./resources/password-data")
 
 (def write-types {:txt {:ext ".txt" :path (str password-data-path "/txt") :data-func "data-newline" :append true}
@@ -26,8 +29,8 @@
 (defn build-trie 
   "Builds a trie from a vector of strings - returns persistent array map 
   with chars as keys and {:t 1} in map where string terminates"
-  [coll]
-  (reduce add-entry {} coll))
+  ([coll] reduce add-entry {} coll)
+  ([coll result] reduce add-entry result coll))
 
 (defn file-dir 
   "Returns canoncial path of a given path"
@@ -39,52 +42,63 @@
   [letter]
   (or (re-find (re-pattern "[^\\w\\s]") (str letter)) false))
 
-;; IO - individual files ;;
-(defn str-to-filename [filename write-type]
+(defn str-to-filename [write-type filename]
   "Creates valid File type class from a filename string + write-type" 
   (io/file (file-dir (:path write-type)) (str filename (:ext write-type))))
-
-(defn first-letter-filename 
-  "Reads first character of a given string. If it is an illegal filename (i.e. *-!), it passes 'special-chars' to str-to-filename. Otherwise pass first character to str-to-filename e.g. 'apple' becomes 'a'"
-   [line write-type]
-   (let [first-char (str (first line))]
-   (if (illegal-starting-char? first-char)
-      (str-to-filename special-chars write-type) 
-      (str-to-filename (lower-case first-char) write-type))))
 
 (defn data-newline [line write-type]
   (str line "\r\n"))
 
+(defn compact-line [line]
+  (s/replace (str line) #" " ""))
+
+(defn set-data [write-type line]
+  (let [func (symbolize write-type)]
+  ((resolve (symbol (:data-func write-type))) line write-type)))
+
+;; IO ;;
+(defn first-letter-filename 
+  "Reads first character of a given string. If it is an illegal filename (i.e. *-!), it passes 'special-chars' to str-to-filename. Otherwise pass first character to str-to-filename e.g. 'apple' becomes 'a'"
+   [write-type line]
+   (let [first-char (str (first line))]
+   (if (illegal-starting-char? first-char)
+      (str-to-filename write-type special-chars) 
+      (str-to-filename write-type (lower-case first-char)))))
+
 (defn update-trie [line write-type]
-  (let [read-file (str-to-filename (first line) write-type)]
+  (let [read-file (str-to-filename write-type (first line))]
   (if (.exists read-file) 
       (add-entry (read-string (slurp read-file)) line)
       (add-entry {} line))))
 
-(defn set-data [line write-type]
-  ((resolve (symbol (:data-func write-type))) line write-type))
-
 (defn write-to-file 
-   [write-file data write-type] 
-       (spit write-file data :append (:append write-type)))
+   [file data write-type] 
+       (spit file data :append (:append write-type)))
 
-(defn write-first-letter-file 
+(defn save-record [write-type line]
+  (write-to-file (first-letter-filename write-type line) (set-data write-type line) write-type))
+
+(defn update-records [write-type lines]
+  (dorun (map #(save-record write-type %) lines)))
+
+(defn first-letter-file 
    [read-file write-type] 
     (with-open [r (io/reader read-file)]
-     (doseq [line (line-seq r)]
-       (let [compacted-line (s/replace (str line) #" " "")]
-       (let [write-file (first-letter-filename compacted-line write-type)]
-       (write-to-file write-file (set-data compacted-line write-type) write-type))))))
+     (->> (line-seq r)
+	 (map compact-line) 
+	 (partition-all 1000)
+	 (#(doseq [lines %]
+	    (update-records write-type lines))))))
 
 ;; IO - Directories ;;
-(defn write-directory-to-files
+(defn directory-to-files
   "Reads each filepath in a given directory into a seq and calls given function on it with a write-type if filepath is a file" 
   [read-dir write-type]
     (let [files (file-seq (io/file (file-dir read-dir)))]
     (doseq [f files] 
       (prn (str "Parsing: " f))
       (if-let [isFile (.isFile f)]
-	(write-first-letter-file f write-type)))))
+	(first-letter-file f write-type)))))
 
 ;; API
 (defn compile-directory-to-tries 
@@ -93,9 +107,9 @@ Requires existence of ./resources/password-data/ + txt + vectors + tries as prer
   [directory]
   (dosync 
     (prn "Compiling to txt files")
-    (write-directory-to-files directory (:txt write-types))
+    (directory-to-files directory (:txt write-types))
     (prn "Building tries from txt files")
-    (write-directory-to-files (get-in write-types [:txt :path]) (:trie write-types))
+    (directory-to-files (get-in write-types [:txt :path]) (:trie write-types))
     (prn "Successfully completed")))
 
 (defn find-repo [word]

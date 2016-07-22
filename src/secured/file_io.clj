7ede8f2034f7f8d1c8be2554d3d2025ae55db910
nrakochy <nick.rakochy@gmail.com>
2016-07-21 23:38:49 -0500

    Added batch processing. Removed all lets. Still getting SO error on some txt files, but functional

--- src/secured/file_io.clj
+++ src/secured/file_io.clj
@@ -5,29 +5,20 @@
 (def password-data-path "./resources/password-data")
 
 (def write-types {:txt {:ext ".txt" :path (str password-data-path "/txt") :data-func "data-newline" :append true :async false}
-		  :trie {:ext ".clj" :path (str password-data-path "/tries") :data-func "update-trie" :append false :async true}})
+		  :trie {:ext ".txt" :path (str password-data-path "/tries") :data-func "update-trie" :append false :async true}})
 
 (def special-chars "special-chars")
 
 ;;; PURE ;;;
-(defn update-record 
-    "Funtion adds :t 1 to result if not yet contained in trie. Otherwise, return result"
-    [result record item]
-    (if (not (contains? record :t))
-      (update-in result (seq item) assoc :t 1)
-      result))
-
 (defn add-entry [result item]
-  (let [record (get-in result item)]
-  (if record
-    (update-record result record item)
-    (assoc-in result (get-in result item item) {:t 1}))))
+  (if (get-in result item) 
+    (update-in result (apply str (seq item)) assoc :t 1)
+    (assoc-in result (get-in result (apply str item) (apply str item)) {:t 1})))
 
 (defn build-trie 
   "Builds a trie from a vector of strings - returns persistent array map 
   with chars as keys and {:t 1} in map where string terminates"
-  ([coll] reduce add-entry {} coll)
-  ([coll result] reduce add-entry result coll))
+  [existing-records coll] (reduce add-entry existing-records coll))
 
 (defn file-dir 
   "Returns canoncial path of a given path"
@@ -50,23 +41,20 @@
   (s/replace (str line) #" " ""))
 
 (defn set-data [write-type data]
-  (let [func (symbolize write-type)]
-  ((resolve (symbol (:data-func write-type))) data write-type)))
+  ((resolve (symbol (:data-func write-type))) data write-type))
 
 ;; IO ;;
 (defn first-letter-filename 
   "Reads first character of a given string. If it is an illegal filename (i.e. *-!), it passes 'special-chars' to str-to-filename. Otherwise pass first character to str-to-filename e.g. 'apple' becomes 'a'"
    [write-type line]
-   (let [first-char (str (first line))]
-   (if (illegal-starting-char? first-char)
+   (if (illegal-starting-char? (str (first line)))
       (str-to-filename write-type special-chars) 
-      (str-to-filename write-type (lower-case first-char)))))
+      (str-to-filename write-type (lower-case (str (first line))))))
 
 (defn update-trie [coll write-type]
-  (let [read-file (str-to-filename write-type (first (first coll)))]
-  (if (.exists read-file) 
-      (build-trie (read-string (slurp read-file)) coll)
-      (build-trie coll))))
+  (if (.exists (str-to-filename write-type (first (first coll)))) 
+      (build-trie (read-string (slurp (str-to-filename write-type (first (first coll))))) coll)
+      (build-trie {} coll)))
 
 (defn write-to-file 
    [file data write-type] 
@@ -76,8 +64,7 @@
   (write-to-file (first-letter-filename write-type line) (set-data write-type line) write-type))
 
 (defn save-batch [write-type coll]
-  (let [first-record (first coll)]
-  (write-to-file (first-letter-filename write-type first-record) (set-data write-type coll) write-type)))
+  (write-to-file (first-letter-filename write-type (first coll)) (set-data write-type coll) write-type))
 
 (defn persist [write-type lines]
   (cond
@@ -89,19 +76,33 @@
     (with-open [r (io/reader read-file)]
      (->> (line-seq r)
 	 (map compact-line) 
-	 (partition-all 10000)
+	 (partition-all 5000)
 	 (#(doseq [lines %]
 	    (persist write-type lines))))))
+
+(defn process-in-mem 
+   [write-type read-file] 
+    (with-open [r (io/reader read-file)]
+     (->> (doall (line-seq r))
+	  (persist write-type))))
 
 ;; IO - Directories ;;
 (defn directory-to-files
   "Reads each filepath in a given directory into a seq and calls given function on it with a write-type if filepath is a file" 
   [read-dir write-type]
-    (let [files (file-seq (io/file (file-dir read-dir)))]
-    (doseq [f files] 
+    (doseq [f (file-seq (io/file (file-dir read-dir)))] 
       (prn (str "Parsing: " f))
-      (if-let [isFile (.isFile f)]
-	(first-letter-file f write-type)))))
+      (if (.isFile f)
+	(first-letter-file f write-type))))
+
+;; IO - Directories ;;
+(defn directory-to-files-in-mem
+  "Reads each filepath in a given directory into a seq and calls given function on it with a write-type if filepath is a file" 
+  [read-dir write-type]
+    (doseq [f (file-seq (io/file (file-dir read-dir)))] 
+      (prn (str "Parsing: " f))
+      (if (.isFile f)
+	  (process-in-mem write-type f))))
 
 ;; API
 (defn compile-directory-to-tries 
@@ -120,4 +121,4 @@ Requires existence of ./resources/password-data/ + txt + vectors + tries as prer
   (let [trie-type (:trie write-types)]
   (if (illegal-starting-char? first-char)
     (read-string (slurp (str-to-filename trie-type special-chars)))
-    (read-string (slurp (str-to-filename trie-type first-char)))))))
+    (read-string (slurp (str-to-filename trie-type first-char)))))

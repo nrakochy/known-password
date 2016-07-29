028862f69e6114d4470871e9011a640d5cc0a86e
nrakochy <nick.rakochy@gmail.com>
2016-07-28 22:19:15 -0500

    Documentation

--- src/secured/core.clj
+++ src/secured/core.clj
@@ -3,11 +3,13 @@
   (:require [secured.file-io :refer [compile-directory-to-tries find-repo]]))
 
 (defn compile-password-tries 
-  "A CLI utility for compiling a directory of txt files of passwords (newline delimited) into a directory of clojure tries. Requires project_root/resources/password-data/ + txt/ vectors/ tries/ as prerequisite directories"
+  "A CLI utility for compiling a directory of txt files of passwords (newline delimited) into a directory of clojure tries. Requires project_root/resources/password-data/ + txt/ & tries/ as prerequisite directories"
   [directory]
   (compile-directory-to-tries directory))
 
 (defn check-password [word]
+  "Takes a string and returns a map with :related-passwords in a vector and known-password? with a boolean- 
+   (check-password "01234567");;-> {:known-password? true :related-passwords ["01234567" "0123456789"]"
  (let [trie (find-repo word)]
  (let [related-passwords {:related-passwords (into [] (search trie word))}]
   (if (present-in-trie? trie word)

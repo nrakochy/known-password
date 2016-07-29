ac3bcdb4e9d511613f2353b2558a4e14e53bb309
nrakochy <nick.rakochy@gmail.com>
2016-07-28 22:10:44 -0500

    Moved some test methods into correct namespace. README

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # secured
 
-A Clojure library designed to ... well, that part is up to you.
+A static trie builder/searcher of known-passwords 
 
 ## Usage
+The API is exposed in secured.core, with two methods -- `compile-password-tries` and `check-password`. See `(source method-name)`
+from the REPL for usage instructions.
 
-FIXME
+Tests can be run with `lein test`
+
+## TODO 
+* Revise the in-memory trie-builder 
+* Make `resources/passwords-data` path ready for uberjar-ing 
+* Rename from secured to known-password
 
 ## License
 
-Copyright © 2016 FIXME
+Copyright © 2016 Nick Rakochy 
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- test/secured/trie_test.clj
+++ test/secured/trie_test.clj
@@ -2,32 +2,6 @@
   (:require [clojure.test :refer :all]
             [secured.trie :refer :all]))
 
-(deftest returns-map-with-key-plus-value-appended-to-vec-given-fn-add-entry-test
-  (testing "Inserts characters as keys of a given word and {:t 1} when given word terminates" 
-    (let [example "test"]  
-    (let [result {\t {\e {\s {\t {:t 1}}}}}]
-    (is (= result (add-entry {} example)))))))
-
-(deftest traverses-trie-before-adding-new-characters-fn-add-entry-test
-  (testing "Traverses trie before adding new characters"
-    (let [example "tester"]  
-    (let [starting {\t {\e {\s {\t {:t 1}}}}}]
-    (let [result {\t {\e {\s {\t {:t 1 \e {\r {:t 1}}}}}}}]
-    (is (= result (add-entry starting example))))))))
-
-(deftest adds-terminating-key-value-if-full-word-already-exists-in-trie-fn-add-entry-test
-  (testing "Updates existing key-value map with {:t 1} if string already exists in trie"
-    (let [example "te"]  
-    (let [starting {\t {\e {\s {\t {:t 1}}}}}]
-    (let [result {\t {\e {:t 1 \s {\t {:t 1}}}}}]
-    (is (= result (add-entry starting example))))))))
-
-(deftest builds-trie-from-vector-of-strings-fn-build-trie-test
-  (testing "Builds trie"
-    (let [example ["test" "testing" "testin"]]  
-    (let [result {\t {\e {\s {\t {:t 1 \i {\n {\g {:t 1} :t 1}}}}}}}]
-    (is (= result (build-trie example)))))))
-
 (deftest finds-branch-with-matching-prefix-fn-find-branch-test
   (testing "Returns hash with matching prefix and the children branch of said prefix"
     (let [example "test"]
